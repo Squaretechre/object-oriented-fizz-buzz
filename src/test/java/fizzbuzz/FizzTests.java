2785e7f202441edb6ac1850d3035a2e4740bacee
Dan Parkin <danparkin@192.168.1.103>
2018-10-29 19:52:40 +0000

    add parameterised test for fizz

--- src/test/java/fizzbuzz/FizzTests.java
+++ src/test/java/fizzbuzz/FizzTests.java
@@ -1,13 +1,30 @@
 package fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class FizzTests {
+
     @Test
-    public void fizz_when_number_evenly_divisible_by_3() {
+    @Parameters({
+            "3",
+            "6",
+            "9",
+            "30",
+            "33",
+            "36",
+            "39",
+            "66",
+            "99",
+
+    })
+    public void fizz_when_number_evenly_divisible_by_3(int number) {
         var fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.forNumber(3));
+        assertEquals("Fizz", fizzBuzz.forNumber(number));
     }
 }
